36cfc8f5d0cb830ea5dd4b50417fc0ff8e83bc68
Prabhu Kumar <prabhukumar636@gmail.com>
2022-06-22 23:17:46 +0530

    BubbleSort and reverse array

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,7 +4,9 @@ using namespace std;
 void BubbleSort(int arr[], int n){
     for(int i=0; i<=n; i++){
         for(int j=0; j<=n-1-i; j++){
-            swap(arr[j], arr[j+1]);
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
         }
     }
 
@@ -12,6 +14,22 @@ void BubbleSort(int arr[], int n){
         cout<<arr[i]<<" ";
     }
 }
+
+//2nd Approach
+
+// void BubbleSort(int arr[], int n){
+//     for(int i=n; i>0; i--){
+//         for(int j=0; j<i-1; j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j], arr[j+1]);
+//             }
+            
+//         }
+//     }
+//     for(int i=0; i<=n-1; i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
 int main(){
     int arr[]={8,5,6,8,4,2,5};
     int size=sizeof(arr)/sizeof(arr[0]);
